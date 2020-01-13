8d86f8bf8832c3adbe82467c2a3d6167567c1496
ricky wahba <rjwahba@gmail.com>
2020-01-13 17:00:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,7 +3,7 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-  if name == collection[0][:name]
+  if name.in?(collection)
     return name
   end
 end
